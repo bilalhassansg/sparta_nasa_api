fd49b827fbff74d4a6ec1072a7e3c577bc0de0d1
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-04-16 15:00:51 +0100

    get three services to have running tests

--- lib/services/nasa_feed.rb
+++ lib/services/nasa_feed.rb
@@ -1,22 +1,31 @@
 require 'httparty'
 require 'json'
 
-class LookupNASA
+class FeedNASA
   include HTTParty
 
   def initialize
     @api_key = '7g3sHlv45g5EMaDJvr9FXg4uT4weGXJmOd87x7BG'
   end
 
-
   base_uri 'https://api.nasa.gov/neo/rest/v1/neo/'
 
-  def get_lookup(start_date, end_date)
-    @lookup_data = JSON.parse(self.class.get("/#{lookup}?api_key=#{@api_key}").body) # Finish !
+  def get_feed(feed)
+    @feed_data = JSON.parse(self.class.get("/#{feed}?api_key=#{@api_key}").body) # Finish !
   end
 
-  def get_status_code_from_body_response
-    @lookup_data['status']
+  def get_neo_id
+    @feed_data['neo_reference_id']
   end
+
+  def get_estimated_diameter_max
+    @feed_data['estimated_diameter']['kilometers']['estimated_diameter_max']
+  end
+
+  def get_estimated_diameter_min_meter
+    @feed_data['estimated_diameter']['meters']['estimated_diameter_min']
+  end
+
+
 
 end
--- spec/browse_spec.rb
+++ spec/browse_spec.rb
@@ -1,7 +1,42 @@
 require 'spec_helper'
 
-describe 'BrowseSpec' do
-  context 'Requesting information on the browse page works correctly' do
+describe 'NASABrowse' do
+  context 'Requesting information on the Nasa Browse page' do
+
+    before(:all) do
+      @nasa_browse = Nasaio.new.browse_data
+      @nasa_browse.get_browse_from_page('2021277')
+    end
+
+    it 'should return the neo_id to equal 2021277' do
+      expect(@nasa_browse.get_neo_id).to eq '2021277'
+    end
+
+    it 'should return absolute_magnitude_h as a Float' do
+      expect(@nasa_browse.get_absolute_magnitude_h).to be_a(Float)
+    end
+
+    it 'should return estimated_diameter:kilometers:estimated_diameter_min and to equal 1.6770846216' do
+      expect(@nasa_browse.get_estimated_diameter_min).to be_a(Float)
+      expect(@nasa_browse.get_estimated_diameter_min).to eq 1.6770846216
+    end
+
+    it 'should return is_potentially_hazardous_asteroid as a true/false and return false' do
+      expect(@nasa_browse.get_is_potentially_hazardous_asteroid).to eq false
+      expect(@nasa_browse.get_is_potentially_hazardous_asteroid).to be(true).or be(false)
+    end
+
+    it 'should return close_approach_data as an array' do
+      expect(@nasa_browse.get_close_approach_data).to be_a(Array)
+    end
+
+    it 'should return close_approach_date' do
+      expect(@nasa_browse.get_close_approach_data.first.first.first).to be_a(String)
+    end
+
+    it 'should return close_approach_data:relative_velocity:kilometers_per_second' do
+      expect(@nasa_browse.get_close_approach_data.first['relative_velocity']['kilometers_per_second']).to be_a(String)
+    end
 
   end
 
